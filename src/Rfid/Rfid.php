671570ef4d364203c3934a6ddc34be8ac29be159
carlo-joinpiggy <croxas@joinpiggy.com>
2018-03-08 23:04:20 +0800

    Initial Commit

---
+++ src/Rfid/Rfid.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Carlo Roxas <iam@carloroxas.com>
+ * PHP Serial Reader
+ */
+namespace Rfid;
+
+use lepiaf\SerialPort\SerialPort;
+use lepiaf\SerialPort\Parser\SeparatorParser;
+use lepiaf\SerialPort\TTYConfigure;
+
+
+class Rfid
+{
+    private $serialPort;
+    private $tty;
+
+    function __construct($tty)
+    {
+        $this->serialPort = new SerialPort(new SeparatorParser(), new TTYConfigure());
+
+        if (isset($tty)) {
+            $this->tty = $tty;
+        }
+    }
+
+    public function openSerial($tty)
+    {
+        $this->serialPort->open($tty);
+    }
+
+    public function readFromSerial()
+    {
+        $data = $this->serialPort->read();
+
+        return $data;
+    }
+
+    public function closeSerial()
+    {
+        $this->serialPort->close();
+    }
+}
